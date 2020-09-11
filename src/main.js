b53d81b899a7174e87b3418962cb5346989e9069
donnymays <dtmays2@gmail.com>
2020-09-11 08:35:23 -0700

    created currency-service.js with GET url tested in postman

--- src/main.js
+++ src/main.js
@@ -2,3 +2,10 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyService from './currency-service.js';
+
+$(document).ready(function() {
+  $().click(function() {
+
+  });
+});
