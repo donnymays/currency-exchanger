35bf6dad93c715b0edb7c9b9b65dd08483e26513
donnymays <dtmays2@gmail.com>
2020-09-11 11:23:49 -0700

    more work on main.js created new branch to try simplified approach

--- src/main.js
+++ src/main.js
@@ -2,47 +2,42 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrencyService from './currency-service.js';
+// import CurrencyService from './currency-service.js';
 import Converter from './convert.js';
 
 
 
 
 
-$(document).ready(function() {
-  $('#exchangeButton').click(function() {
-    
+$(document).ready(function () {
+  $('#exchangeButton').click(function () {
+
     let userCurrency = parseInt($('#amount').val());
     let selectedCurrency = $(".currencySelect").val();
-    let convertToInput = `response.conversion_rates.${selectedCurrency}`;
-    console.log(`response.conversion_rates.CAD`);
-    
+    let convertToInput = `${response.conversion_rates.selectedCurrency}`;
+    console.log(convertToInput);
+
     let converter = new Converter(userCurrency, convertToInput);
     converter.convertCurrency();
-  
+
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
 
     function getElements(response) {
-      if (response.converion_rates) {
-        $('.showResults').text(`${userCurrency} in USD is worth ${converter.convertedCurrency}`)
-      } else {
-        $('.showErrors').text(`There was an error: ${response}`);
-      }
+      $('.showResults').text(`${userCurrency} in USD is worth ${converter.convertedCurrency}`)
+      console.log(userCurrency);
+      console.log(converter.convertCurrency);
     }
-    
-    async function makeApiCall() {
-      const response = await CurrencyService.exchangeCurrency();
-      getElements(response);
-    }
-
-    makeApiCall();
-   
-
-    
-    
-   
-
- 
-
-
   });
 });
